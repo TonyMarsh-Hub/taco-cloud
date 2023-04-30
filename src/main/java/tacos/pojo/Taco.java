e152fd0f992260df66835cf0035729f402c190ae
tonymarsh <tonymarsh0331@gmail.com>
2023-04-30 19:08:58 +0800

    完成3.2节部分，使用SpringDataJDBC简化数据处理

--- src/main/java/tacos/pojo/Taco.java
+++ src/main/java/tacos/pojo/Taco.java
@@ -3,22 +3,26 @@ package tacos.pojo;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.util.Date;
 import java.util.List;
 
 @Data
+@Table // 可选，如果不加，Spring Data JPA 会使用类名作为表名, 一般在类名和表面不一致的时候使用
 public class Taco {
 
+    @Id
     private Long id;
 
     private Date createdAt;
 
     @NotNull
-    @Size(min=5, message="Name must be at least 5 characters long")
+    @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
 
     @NotNull
-    @Size(min=1, message="You must choose at least 1 ingredient")
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
 }
