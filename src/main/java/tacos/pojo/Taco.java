626b0c02c58156f208e82b68280a758205b06ac2
tonymarsh <tonymarsh0331@gmail.com>
2023-05-01 21:46:03 +0800

    Back to JPA, and add security dependency

--- src/main/java/tacos/pojo/Taco.java
+++ src/main/java/tacos/pojo/Taco.java
@@ -1,26 +1,32 @@
 package tacos.pojo;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
+import lombok.*;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
 public class Taco {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Date createdAt;
+
     @NotNull
-    @Size(min=5, message="Name must be at least 5 characters long")
+    @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
 
-    private Date createdAt = new Date();
-
-    @Size(min=1, message="You must choose at least 1 ingredient")
-    private List<Ingredient> ingredients = new ArrayList<>();
-
-    public void addIngredient(Ingredient ingredient) {
-        this.ingredients.add(ingredient);
-    }
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
+    @ManyToMany
+    @ToString.Exclude
+    private List<Ingredient> ingredients;
 }
--- src/main/java/tacos/pojo/TacoOrder.java
+++ src/main/java/tacos/pojo/TacoOrder.java
@@ -1,12 +1,11 @@
 package tacos.pojo;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.Data;
+import lombok.*;
 import org.hibernate.validator.constraints.CreditCardNumber;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -14,15 +13,18 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-@Data
-@Document
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
 public class TacoOrder implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
-    private String id;
-
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private Date placedAt;
     @NotBlank(message = "Delivery name is required")
     private String deliveryName;
@@ -49,6 +51,8 @@ public class TacoOrder implements Serializable {
     @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
 
+    @OneToMany
+    @ToString.Exclude
     private List<Taco> tacos = new ArrayList<>();
 
     public void addTaco(Taco taco) {
