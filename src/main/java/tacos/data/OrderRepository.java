613eee27edf4e2bb9dfab811484d9a124b1dbe7a
tonymarsh <tonymarsh0331@gmail.com>
2023-05-01 17:26:11 +0800

    处理NoSQL-MongoDB

--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -3,9 +3,7 @@ package tacos.data;
 import org.springframework.data.repository.CrudRepository;
 import tacos.pojo.TacoOrder;
 
-import java.util.UUID;
 
-
-public interface OrderRepository extends CrudRepository<TacoOrder, UUID> {
+public interface OrderRepository extends CrudRepository<TacoOrder, String> {
 
 }
--- src/main/java/tacos/pojo/Taco.java
+++ src/main/java/tacos/pojo/Taco.java
@@ -1,43 +1,26 @@
 package tacos.pojo;
 
-import com.datastax.oss.driver.api.core.uuid.Uuids;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-import org.springframework.data.cassandra.core.cql.Ordering;
-import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
-import org.springframework.data.cassandra.core.mapping.Column;
-import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
-import org.springframework.data.cassandra.core.mapping.Table;
-import tacos.pojo.UDT.IngredientUDT;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 
 @Data
-@Table("tacos")
 public class Taco {
 
-    @PrimaryKeyColumn(type = PrimaryKeyType.PARTITIONED)
-    private UUID id = Uuids.timeBased();
-
     @NotNull
-    @Size(min = 5, message = "Name must be at least 5 characters long")
+    @Size(min=5, message="Name must be at least 5 characters long")
     private String name;
 
-    @PrimaryKeyColumn(type = PrimaryKeyType.CLUSTERED,
-            ordering = Ordering.DESCENDING)
     private Date createdAt = new Date();
 
-    @Size(min = 1, message = "You must choose at least 1 ingredient")
-    @Column("ingredients")
-    private List<IngredientUDT> ingredients = new ArrayList<>();
+    @Size(min=1, message="You must choose at least 1 ingredient")
+    private List<Ingredient> ingredients = new ArrayList<>();
 
     public void addIngredient(Ingredient ingredient) {
-        this.ingredients.add(
-                new IngredientUDT(ingredient.getName(), ingredient.getType())
-        );
+        this.ingredients.add(ingredient);
     }
 }
