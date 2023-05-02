61b775b2089cf47e8d34ee565396b52f0632255c
tonymarsh <tonymarsh0331@gmail.com>
2023-05-02 15:01:18 +0800

    :fire: remove all files , switch to source code provided by the book.

--- src/main/java/tacos/data/IngredientRepository.java
+++
@@ -1,6 +0,0 @@
-package tacos.data;
-
-import org.springframework.data.repository.CrudRepository;
-import tacos.pojo.Ingredient;
-
-public interface IngredientRepository extends CrudRepository<Ingredient, String> {}
--- src/main/java/tacos/data/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package tacos.data;
-
-import org.springframework.data.repository.CrudRepository;
-import tacos.pojo.TacoOrder;
-
-
-public interface OrderRepository extends CrudRepository<TacoOrder, String> {
-
-}
